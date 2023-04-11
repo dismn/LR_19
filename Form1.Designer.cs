6ac14515f7fa9a6cf7ee9b3f0d8a70ec046f77c2
admin <admin@DESKTOP-GARPRVJ>
2023-04-11 14:21:05 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace LR_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.PrintSequenceButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(285, 121);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(235, 22);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // PrintSequenceButton
+            // 
+            this.PrintSequenceButton.BackColor = System.Drawing.SystemColors.Info;
+            this.PrintSequenceButton.Font = new System.Drawing.Font("Monotype Corsiva", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PrintSequenceButton.Location = new System.Drawing.Point(325, 182);
+            this.PrintSequenceButton.Name = "PrintSequenceButton";
+            this.PrintSequenceButton.Size = new System.Drawing.Size(147, 33);
+            this.PrintSequenceButton.TabIndex = 1;
+            this.PrintSequenceButton.Text = "Обробка";
+            this.PrintSequenceButton.UseVisualStyleBackColor = false;
+            this.PrintSequenceButton.Click += new System.EventHandler(this.PrintSequenceButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(149, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Обробка рядку(видалення чисел та заміна на \"грн\")";
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(285, 266);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(235, 22);
+            this.OutputTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PrintSequenceButton);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button PrintSequenceButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿//Copyright (c), Semeniuta Diana 202 TN, 2023
+/*Задана послідовність символові. Роздрукувати цю послідовність:
+а) витерши з неї всі цифри;
+б) замінивши буквосполучення 'карб' на 'грн'.*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR_19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PrintSequenceButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string input = InputTextBox.Text;
+
+                // видаляємо всі цифри з рядка
+                string result = new string(input.Where(c => !Char.IsDigit(c)).ToArray());
+
+                // замінюємо буквосполучення "карб" на "грн"
+                result = result.Replace("карб", "грн");
+
+                // виводимо результат
+                OutputTextBox.Text = result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
